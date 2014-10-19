79fefdca90bd411f3dd47a3caa6c79bf9d6255d2
Levi <nadaaver@bafodepimenta.com>
2014-10-18 22:36:53 -0300

    [levi] - inserts inseridos. entendeu? huahua

--- DOCAL-core/src/main/java/br/unifor/ads/DOCAL_core/dao/DietaDAO.java
+++ DOCAL-core/src/main/java/br/unifor/ads/DOCAL_core/dao/DietaDAO.java
@@ -1,11 +1,47 @@
 package br.unifor.ads.DOCAL_core.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import br.unifor.ads.DOCAL_core.entity.Dieta;
 
 public class DietaDAO {
 
 	public void insert(Dieta dieta) {
-		//COMO QUE INSERE FOREIGN KEY, PABLO???
+		Connection connection = null;
+		try {
+			connection = ConnectionPool.getConnection();
+			String sql = "insert into dieta "
+					+ "(usuario_id, nome, carboidratos, proteinas, gorduras, calorias) "
+					+ "values (?, ?, ?, ?, ?, ?)";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setInt(1, dieta.getUsuario_id().getId());
+			statement.setString(2, dieta.getNome());
+			statement.setFloat(3, dieta.getCarboidratos());
+			statement.setFloat(4, dieta.getProteinas());
+			statement.setFloat(5, dieta.getGorduras());
+			statement.setFloat(6, dieta.getCalorias());
+			statement.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Nao foi possivel inserir o usuario!");
+		} finally {
+			tryCloseConnection(connection);
+		}
 	}
+	
+	private void tryCloseConnection(Connection connection) {
+		try {
+			if(connection != null && !connection.isClosed()) {
+				connection.close();
+			} 
+		}catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Nao foi possivel fechar a conexao?");
+		}
+	}
+	
 
 }
--- DOCAL-core/src/main/java/br/unifor/ads/DOCAL_core/dao/RefeicaoDAO.java
+++ DOCAL-core/src/main/java/br/unifor/ads/DOCAL_core/dao/RefeicaoDAO.java
@@ -1,10 +1,45 @@
 package br.unifor.ads.DOCAL_core.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import br.unifor.ads.DOCAL_core.entity.Refeicao;
 
 public class RefeicaoDAO {
 
 	public void insert(Refeicao refeicao) {
-		//COMO QUE INSERE FOREIGN KEY, PABLO???
+		Connection connection = null;
+		try {
+			connection = ConnectionPool.getConnection();
+			String sql = "insert into refeicao "
+					+ "(usuario_id, nome, carboidratos, proteinas, gorduras, calorias) "
+					+ "values (?, ?, ?, ?, ?, ?)";
+			PreparedStatement statement = connection.prepareStatement(sql);
+			statement.setInt(1, refeicao.getUsuario_id().getId());
+			statement.setString(2, refeicao.getNome());
+			statement.setFloat(3, refeicao.getCarboidratos());
+			statement.setFloat(4, refeicao.getProteinas());
+			statement.setFloat(5, refeicao.getGorduras());
+			statement.setFloat(6, refeicao.getCalorias());
+			statement.execute();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Nao foi possivel inserir o usuario!");
+		} finally {
+			tryCloseConnection(connection);
+		}
+	}
+	
+	private void tryCloseConnection(Connection connection) {
+		try {
+			if(connection != null && !connection.isClosed()) {
+				connection.close();
+			} 
+		}catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Nao foi possivel fechar a conexao?");
+		}
 	}
 }
